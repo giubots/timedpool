8224881da4efa12db0ed863fe33da7b426b14301
giubots <giubots@protonmail.com>
2021-12-31 15:26:01 +0000

    prepare for upload

--- timedpool/__init__.py
+++ timedpool/__init__.py
@@ -1,5 +1,5 @@
 """A dict with a maximum size whose elements are deleted after a delay."""
 
-__version__ = '0.1.2'
+__version__ = '0.1.0'
 
 from timedpool.timedpool import TimedPool, FullException
